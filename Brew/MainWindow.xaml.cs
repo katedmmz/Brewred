e2c70a16c8d9b1240acd5e55f3928297a4155060
Ekaterina Shubina <katedmmz@mail.ru>
2021-05-27 16:39:05 +0300

    Добавьте файлы проекта.

---
+++ Brew/MainWindow.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Collections.ObjectModel;
+
+namespace Brew
+{
+    // Класс Пивоварной компании со свойствами
+    /* Название
+     * Страна
+     * Дата Основания
+     * Имя основателя
+     * Количество продукции
+     * Средний рейтиинг
+     * Что производится
+     */
+    public class Brewery
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public DateTime DateOfCreation { get; set; }
+        public string FounderName { get; set; }
+        public int VarietyCount { get; set; }
+        public double AvgRating { get; set; }
+        public string Products { get; set; }
+    }
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Коллекция пивоварен
+        ObservableCollection<Brewery> breweries;
+        public MainWindow()
+        {
+            InitializeComponent();
+            breweries = new ObservableCollection<Brewery>();
+
+            // Если файл с таблицей существует
+            if (new FileInfo("table.txt").Exists)
+            {
+                // Считываем строки и проходимся по ним
+                foreach(string line in File.ReadAllLines("table.txt"))
+                {
+                    // Разделяем строку по символу-разделителю
+                    // Вид строки в файле: Name|Country|dd,MM,yyyy|FounderName|00|0,00|a;b;c;
+                    string[] data = line.Split('|');
+                    // Разделяем дату по дню, месяцу и году
+                    int[] date = data[2].Split(',').Select(int.Parse).ToArray();
+                    // Заполняем пивоварню
+                    Brewery newline = new Brewery
+                    {
+                        Name = data[0],
+                        Country = data[1],
+                        DateOfCreation = new DateTime(date[2], date[1], date[0]), // Дата в фромате YYYY, MM, DD
+                        FounderName = data[3],
+                        VarietyCount = int.Parse(data[4]),
+                        AvgRating = float.Parse(data[5]),
+                        Products = data[6]
+                    };
+                    // Добавляем ее в коллекцию
+                    breweries.Add(newline);
+                }
+                // Очищаем файл
+                File.WriteAllText("table.txt", "");
+            }
+            // Если файла нет, то создаем его
+            else
+            {
+                FileStream fs = File.Create("table.txt");
+                fs.Close();
+            }
+            // Устанавливаем источних данных для DataGrid
+            BreweryGrid.ItemsSource = breweries;
+        }
+
+        // Нажатие на кнопку Добавить
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // создается окно для добавления
+            wndCreate add = new wndCreate("Добавить");
+
+            // Инициализируем как диалогове окно
+            add.ShowDialog();
+
+            // Если мы нажали в окне добавление на "Добавить", то добавляем элемент в коллекцию
+            if (add.DialogResult ?? true)
+            {
+                breweries.Add(add.Item);
+            }
+        }
+
+        // Нажатие на кнопку Изменить
+        private void EditBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Если не выбрана строка, выводим сообщение
+            if(!(BreweryGrid.SelectedItem is Brewery row))
+            {
+                MessageBox.Show("Выберите строку для изменения.", "Внимание", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                // Создаем окно для изменения и передем в него коллекцию и выбраный ряд
+                wndCreate Edit = new wndCreate("Изменить", breweries, row);
+                // Инициализируем как диалогове окно
+                Edit.ShowDialog();
+            }
+        }
+
+        // Нажатие на кнопку Удалить
+        private void RemoveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Если не выбрана строка, выводим сообщение 
+            if (!(BreweryGrid.SelectedItem is Brewery row))
+            {
+                MessageBox.Show("Выберите строку для изменения.", "Внимание", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBoxResult dlg = MessageBox.Show("Внатуре удалить?", "Опа!", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if(dlg == MessageBoxResult.Yes)
+                    // Удаляем ряд
+                    breweries.Remove(row);
+            }
+        }
+
+        // При закрытии окна сохраняем таблицу в файл
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            foreach(Brewery toAdd in breweries)
+            {
+                File.AppendAllText("table.txt", $"{toAdd.Name}|{toAdd.Country}|{toAdd.DateOfCreation:dd,MM,yyyy}|{toAdd.FounderName}|{toAdd.VarietyCount}|{toAdd.AvgRating:F2}|{toAdd.Products}\n");
+            }
+        }
+    }
+}
---
+++ Brew/wndCreate.xaml.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.Collections.ObjectModel;
+
+namespace Brew
+{
+    /// <summary>
+    /// Логика взаимодействия для wndCreate.xaml
+    /// </summary>
+    public partial class wndCreate : Window
+    {
+        // Создаем переменные коллекции и ряда в глобальной области видимости
+        ObservableCollection<Brewery> breweries; // коллекция пивоварен
+        Brewery row;
+
+        // Конструктор для изменения ряда
+        public wndCreate(string caption, ObservableCollection<Brewery> breweries, Brewery row)
+        {
+            InitializeComponent();
+            // Устанавливаем заголовок окна
+            Title = caption;
+            // Заполняем поля по данным из переданного ряда
+            NameTB.Text = row.Name;
+            CountryTB.Text = row.Country;
+            DateDP.SelectedDate = row.DateOfCreation;
+            FounderTB.Text = row.FounderName;
+            VarietyTB.Text = row.VarietyCount.ToString();
+            AvgRatingTB.Text = string.Format(@"{0:F2}", row.AvgRating);
+            // Создаем ComboBoxItem в котором будет содержаться список выбранных продуктов
+            ComboBoxItem list = new ComboBoxItem();
+            // Выставляем отступы как у остальных строк в ComboBox, потому что надо
+            list.Margin = new Thickness(-5, -2, -5, -3); //толщина рамки 
+            // выставляем ширину как у других элементов
+            list.Width = 158;
+            list.Content = row.Products;
+            // Ставим высоту 0 чтобы элемент был скрыт
+            list.Height = 0;
+            // Ставим галочки
+            foreach (CheckBox cb in ProductCB.Items)
+            {
+                foreach(string a in list.Content.ToString().Split(';'))
+                {
+                    if (cb.Content.ToString() == a.Trim()) cb.IsChecked = true;
+                }
+            }
+            // Добавляем его в ComboBox
+            ProductCB.Items.Add(list);
+            
+            // Делаем его как выбранный, чтобы он отображался в ComboBox
+            ProductCB.SelectedItem = list;
+            this.breweries = breweries;
+            this.row = row;
+            // Переименовываем кнопку
+            AcceptBtn.Content = "Сохранить";
+        }
+
+        // Конструктор для Добавления
+        public wndCreate(string caption)
+        {
+            InitializeComponent();
+            Title = caption;
+        }
+        // Нажатие на CheckBox'ы
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            // Строка выбранных элементов
+            string res = "";
+            ComboBoxItem list = new ComboBoxItem();
+            list.Margin = new Thickness(-5, -2, -5, -3);
+            list.Width = 158;
+
+            // Пытаемся удлаить последний элемент(ранее добавленный ComboBoxItem), чтобы не было ошибки в foreach
+            try
+            {
+                object toDelete = ProductCB.Items[6];
+                ProductCB.Items.Remove(toDelete);
+            } catch(Exception)
+            {
+                //
+            }
+            foreach (CheckBox cb in ProductCB.Items)
+            {
+                // Формируем строку выбранных элементов из combobox и преобразуем их в строку
+                res += (cb.IsChecked ?? true) ? (cb.Content + "; ") : "";
+            }
+            // Записываем строку в значение созданного ComboBoxItem, добавляем его и делаем выбранным
+            list.Content = res;
+            list.Height = 0;
+            ProductCB.Items.Add(list);
+            ProductCB.SelectedItem = list;
+        }
+
+        // Кнопка Добавить/Сохранить
+        private void AcceptBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Регулярные выражения для числа, страны, дробного числа, названия и имени
+            Regex number = new Regex(@"^[1-9][0-9]*$"); // => Целое положительное число начинающееся не с нуля
+            Regex country = new Regex(@"^[A-z][A-Za-z]+.*$"); // => В начале заглавная буква, потом только буквы(1 или более) потом любые символы
+            Regex float_number = new Regex(@"^[0-9][,][0-9]{2}$"); // => число формата X,XX от 0.00 до 9.99
+            Regex name = new Regex(@"^[A-Z][a-z]+.*$"); // => Начинается с большой буквы, потом маленькие(1 или более) потом любые символы
+            Regex founder = new Regex(@"^[A-Z][.][\s][A-Z][a-z]+$"); // => Одна заглавная буква, точка, пробел, одно слово с заглавной буквы
+
+            // Проверяем соответсвия, и в случае несовпадения выводим сообщение и выходим из функции
+            if (!name.IsMatch(NameTB.Text))
+            {
+                MessageBox.Show("Неверный формат данных. (Название)\n\nПример: Beergood, Sinergoff");
+                return;
+            }
+            else if (!country.IsMatch(CountryTB.Text))
+            {
+                MessageBox.Show("Неверный формат данных. (Страна)\n\nПример: Russia, Usa, Brazil");
+                return;
+            }
+            else if (DateDP.SelectedDate == null)
+            {
+                MessageBox.Show("Выберите дату основания.");
+                return;
+            }
+            else if (!founder.IsMatch(FounderTB.Text))
+            {
+                MessageBox.Show("Неверный формат данных. (Основатель)\n\nПример: T. Hanks, R. Teres");
+                return;
+            }
+            else if (!number.IsMatch(VarietyTB.Text))
+            {
+                MessageBox.Show("Количество продуктов должно быть целым положительным числом.");
+                return;
+            }
+            else if (!float_number.IsMatch(AvgRatingTB.Text))
+            {
+                MessageBox.Show("Средний рейтинг должен быть в формате \"X,XX\"( От 0 до 9,99)");
+                return;
+            }
+            else if(ProductCB.Text == "")
+            {
+                MessageBox.Show("Выберите продукцию.");
+                return;
+            }
+            
+            // Если мы в режиме Изменения
+            if (Title == "Изменить")
+            {
+                // Вставляем на место текущего ряда новосозданный элемент
+                breweries.Insert(breweries.IndexOf(row), Item);
+                // Удаляем старый ряд
+                breweries.Remove(row);
+            }
+            // Выходим из диалогового окна
+            DialogResult = true;
+        }
+
+        // Получение элемента из введенных данных
+        public Brewery Item
+        {
+            get
+            {
+                Brewery res = new Brewery
+                {
+                    Name = NameTB.Text.Trim(),
+                    Country = CountryTB.Text.Trim(),
+                    DateOfCreation = DateDP.SelectedDate.Value,
+                    FounderName = FounderTB.Text.Trim(),
+                    VarietyCount = int.Parse(VarietyTB.Text.Trim()),
+                    AvgRating = double.Parse(AvgRatingTB.Text.Trim()),
+                    Products = ProductCB.Text.Trim(),
+                };
+                return res;
+            }
+        }
+    }
+}
